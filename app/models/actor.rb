b8cb306492f908f10bf495681abc1e28edca9ab2
Monica Marziano <mmarziano1@gmail.com>
2019-03-08 00:41:29 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,9 @@ class Actor < ActiveRecord::Base
   end 
 
   def list_roles 
+    self.characters.collect do |character|
+      character.name
+    end 
     return "#{self.characters.name} - #{self.shows.name}"
   end 
 
