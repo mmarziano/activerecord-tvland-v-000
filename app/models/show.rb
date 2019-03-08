4c8164ddffa0d2a1f98d670bcdb9cacb8b82fe5e
Monica Marziano <mmarziano1@gmail.com>
2019-03-08 00:59:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
-  def build_network(self.network)
+  def build_network
     Network.new(self.network.call_letters)
   end 
 
