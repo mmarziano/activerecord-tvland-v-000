38acc071002c46482a884a37a743da7dd7c35835
Monica Marziano <mmarziano1@gmail.com>
2019-03-08 01:07:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def build_network(network)
-   Network.new(network)
+   self.network = Network.new(network)
   end 
 
 end
